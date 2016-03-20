99db90ae4f8ca151c5e3bdbbd764be44b477ba4d
pizzadeesnutz <Arcadio@nbp-208-68.nbp.ruw.rutgers.edu>
2016-03-19 21:20:47 -0400

    Testing Github

--- src/Testing.java
+++ src/Testing.java
@@ -3,7 +3,7 @@ public class Testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Welcome to my Github!!!");
+			System.out.println("Welcome to my Github!!");
 			//follow me for future code examples
 	}
 
