e51daaf9aa04acfd72d473af9097d6b070f55492
pizzadeesnutz <Arcadio@nbp-208-68.nbp.ruw.rutgers.edu>
2016-03-20 20:40:25 -0400

    all changes final

--- src/HowToCast.java
+++ src/HowToCast.java
@@ -1,18 +1,27 @@
+import java.util.Scanner;
 
 public class HowToCast {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 		Object [] test = new Object[5];
 		for(int i = 0; i < test.length; i++){
 			test [i] = true;
 		}
-		test[0] = 1;
+		test[1] = 1;
 		// this line would cause an issue boolean getBoolean = test[1];
 		//should read like this:
-		boolean getBoolean = (boolean) test[1];
+		boolean getBoolean = (boolean) test[0];
 		//a comparison that uses casting
-		if(getBoolean == (boolean) test[1]) System.out.println("They are both true!!!");
+		if(getBoolean == (boolean) test[0]) System.out.println("They are both true!!!");
+		System.out.println("Enter a guess: ");
+		int guess = input.nextInt();
+		//still need to cast
+		//why?
+		//because our array is filled with Objects NOT integers...
+		if(guess == (int) test[1]) System.out.println("They match!!!");
+		else System.out.println("WRONG!!");
+		input.close();
 	}
 
 }
