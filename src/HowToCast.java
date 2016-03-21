86eeb0e2997c191a1c327c099c6ef1831ab9ba59
pizzadeesnutz <Arcadio@nbp-208-68.nbp.ruw.rutgers.edu>
2016-03-20 20:26:57 -0400

    Added a Casting Explanation

---
+++ src/HowToCast.java
@@ -0,0 +1,13 @@
+
+public class HowToCast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Object [] test = new Object[5];
+		for(int i = 0; i < test.length; i++){
+			test [i] = true;
+		}
+		test[0] = 1;
+	}
+
+}
